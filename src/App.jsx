bdca0ebc656d154ea5ee27ec48e39e0a1790c1ef
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-04-02 11:58:51 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header.jsx';
+
+function App() {
+  return (
+    <>
+    <Header />
+    
+    
+    
+    
+    
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+function Header() {
+  return (
+    <header className="bg-amber-400 rounded fixed z-50 w-full ">
+      
+        <h1 className="text-4xl md:text-2xl p-4 font-bold italic text-gray-800"> <span className="text-white font-bold">JA</span>HA</h1>
+        
+      
+    </header>
+  )
+}
+
+export default Header;
