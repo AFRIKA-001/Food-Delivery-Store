e0c73c136505f4db582d3825bcc937d6e7140ebf
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-04-02 20:14:55 +0300

    finished the navBar and applied some stylings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
-
+import { Search,ShoppingCart } from "lucide-react";
 function Header() {
   return (
-    <header className="bg-amber-400 rounded fixed z-50 w-full ">
+    <header className="lg:bg-amber-500 md:bg-amber-300 rounded fixed z-50 w-full flex p-4 items-center">
+    
+           <h1 className="lg:text-3xl md:text-2xl font-bold italic text-gray-800 "> <span className="text-white font-bold">JA</span>HA Foods</h1>
+           <div className="flex mx-auto lg:w-150">
+            <Search className="text-gray-500 bg-white h-9 w-8 rounded-l-sm" />
+           <input type="search" placeholder="Search..." className="bg-white text-gray-800 placeholder:text-gray-500 border border-transparent focus:outline-none  rounded-r-md md:w-full " />
+           </div>
       
-        <h1 className="text-4xl md:text-2xl p-4 font-bold italic text-gray-800"> <span className="text-white font-bold">JA</span>HA</h1>
-        
-      
+        <button className=" h-8 w-8 ml-auto cursor-pointer active:bg-amber-700 rounded mx-4 border shadow-md bg-amber-500 flex text-white">
+          <ShoppingCart /><span className="ml-1 text-stone-900">{0}</span>
+        </button>
+   
+     
     </header>
   )
 }
