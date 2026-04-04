91a531f8f5d0a4f94b1c23ebfc62c2a266879008
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-04-04 13:53:05 +0300

    added a modal component and created a portal in it

---
+++ src/UI/Modal.jsx
@@ -0,0 +1,12 @@
+import {createPortal} from 'react-dom';
+
+function Modal() {
+  return createPortal(
+    <dialog>
+      
+    </dialog>
+  ,document.getElementById("modal")
+  );
+}
+
+export default Modal
