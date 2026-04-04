21a2319279d56c0db252e353b519977fb89d389d
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-04-04 13:17:42 +0300

    wrapped the parent component(app) with the context Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import Header from './components/Header.jsx';
 import MealFetching from './components/MealFetching.jsx';
+import { CartContextProvider } from './store/CartContext.jsx';
 
 function App() {
   return (
-    <>
+    <CartContextProvider>
     <Header />
     <MealFetching />
-    
-    
-    
-    
-    
-    </>
+    </CartContextProvider>
   );
 }
 
