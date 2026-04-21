26a1b50a7f1b447a84eabc844afd7675412db8b1
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-04-21 18:28:01 +0300

    made some styling changes to the checkout page

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -41,15 +41,15 @@ function CheckOut() {
 
         <h2 className='text-center text-2xl lg:text-4xl font-mono pt-20 text-orange-500'>Checkout</h2>
 
-        <form ref={formRef} onSubmit={handleSubmit} className=' rounded flex flex-col lg:h-70 lg:w-100 p-4 md:p-4 lg:p-8 my-10 text-xl mx-auto'>
+        <form ref={formRef} onSubmit={handleSubmit} className='space-y-4 rounded flex flex-col min-h-screen lg:w-100 p-4 md:p-4 lg:p-8 my-10 text-xl mx-auto'>
 
-            <p className='pb-4 text-2xl font-bold'>Total Amount:{formatPrice.format(cartTotal)}</p>
-            <Inputs label='Full Name' id="user-id" name='fullname' type="text" className='rounded border flex' />
-            <Inputs label='E-Mail Address' id="email" name='email' type="email" className='rounded border' />
-            <Inputs label='Street' id="street" name='steet' type="text" className="border flex rounded" />
+            <p className='pb-4 lg:text-2xl font-bold'>Total Amount:{formatPrice.format(cartTotal)}</p>
+            <Inputs label='Full Name' id="user-id" name='fullname' type="text" className='w-full text-sm   rounded border flex' />
+            <Inputs label='E-Mail Address' id="email" name='email' type="email" className='w-full text-sm rounded border' />
+            <Inputs label='Street' id="street" name='steet' type="text" className="w-full text-sm border flex rounded" />
             <div className='lg:flex flex-col gap-4 lg:pt-4 '>
-                <Inputs label='Postal Code' id="postal-code" name='postal' type="text" className='border line-clamp-1 rounded ' />
-                <Inputs label='Phone Number' id="city" type="number" name='phone number' className='border rounded flex' />
+                <Inputs label='Postal Code' id="postal-code" name='postal' type="text" className='w-full text-sm border line-clamp-1 rounded ' />
+                <Inputs label='Phone Number' id="city" type="number" name='phone number' className='w-full text-sm border rounded flex' />
             </div>
              <div className='pt-4 flex gap-6 '>
             <button className='border rounded bg-amber-300 hover:bg-amber-500 active:bg-amber-600'>
