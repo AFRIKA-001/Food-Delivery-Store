abd956bf752429375a80af0b2955cb4cf36a6e63
AFRIKA-001 <shikukugabriel06@gmail.com>
2026-05-27 23:42:17 +0300

    set Toaster globally

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { UserAuthContextProvider } from './store/UserAuthContext.jsx'
 import { SearchBarContextProvider } from './store/SearchBarContext.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './components/Routing.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
 
@@ -15,6 +16,17 @@ createRoot(document.getElementById('root')).render(
       <SearchBarContextProvider>
         <UserProgressContextProvider>
           <CartContextProvider>
+            <Toaster position='top-right' reverseOrder={false} toastOptions={{
+              duration:1000,
+              style:{
+                background: "oklch(14.1% 0.005 285.823))",
+                color: "#000",
+                border: "1px #1e293b",
+                padding: "16px",
+                borderRadius: "16px",
+            }
+          }}
+            />
             <RouterProvider router={router} />
           </CartContextProvider>
         </UserProgressContextProvider>
